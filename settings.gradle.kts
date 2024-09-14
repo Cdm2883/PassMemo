c86f9bd9503b6e41a28110d26e122e1bb0c64d81
Cdm2883 <Cdm2883@hotmail.com>
2024-09-14 21:37:21 +0800

    build: configure the download mirror

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,11 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
     repositories {
+        maven("https://mirrors.huaweicloud.com/repository/maven/")
+        maven("https://maven.aliyun.com/repository/public")
+        maven("https://maven.aliyun.com/repository/google")
         google {
+            @Suppress("UnstableApiUsage")
             mavenContent {
                 includeGroupAndSubgroups("androidx")
                 includeGroupAndSubgroups("com.android")
@@ -16,7 +20,11 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
     repositories {
+        maven("https://mirrors.huaweicloud.com/repository/maven/")
+        maven("https://maven.aliyun.com/repository/public")
+        maven("https://maven.aliyun.com/repository/google")
         google {
             mavenContent {
                 includeGroupAndSubgroups("androidx")
